5f8812addd4c07a036949887d4a2d65a9595dd4c
amitchaud10@gmail.com <amitchaud10@gmail.com>
2023-10-21 10:44:15 +0530

    design

---
+++ src/Component/Pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+          <div className="container-fluid">
+  <div className="" >
+    <img
+      src="../logo192.png"
+      className="card-img-top"
+      alt="Background Image"
+      style={{ height: "450px" }} // Set the image height to 350px
+    />
+  </div>
+</div>
+
+
+        </div>
+    )
+}
+
+export default Home
