27caaf5cb16a26ad641677c0f1c627e564bd6b19
amitchaud10@gmail.com <amitchaud10@gmail.com>
2023-10-21 18:49:52 +0530

    2nd

--- src/Component/Pages/Home.js
+++ src/Component/Pages/Home.js
@@ -1,22 +1,32 @@
 import React from 'react'
-
+import '../Layout.css'
 function Home() {
-    return (
-        <div>
-          <div className="container-fluid">
-  <div className="" >
-    <img
-      src="../logo192.png"
-      className="card-img-top"
-      alt="Background Image"
-      style={{ height: "450px" }} // Set the image height to 350px
-    />
-  </div>
-</div>
-
-
+  return (
+    <div>
+      <div className=" bg_image">
+     <div className='container'>
+      <div className='row'>
+        <div className='col-lg-8 col-md-6 col-sm-12 py-md-5 mt-md-5'>
+        <div class="intro-body ">
+                      <p class="intro-title-top" style={{color:'whitesmoke'}}>23,Mehul Nagar,Indore
+                        <br/> 272002
+                      </p>
+                      <h1 class="intro-title mb-4 " style={{color:'whitesmoke'}}>
+                        <span class="color-b">4BHK </span > house
+                        <br/> Villa
+                      </h1>
+                      <p class="intro-subtitle intro-price ">
+                        <button className='btn btn-outline-primary btn-lg'> <a href=""><span class="price-a "style={{color:'whitesmoke'}}>Buy | ₹ 123456</span></a></button>
+                       
+                      </p>
+                    </div>
         </div>
-    )
+      </div>
+     </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default Home
--- src/Component/header.js
+++ src/Component/header.js
@@ -1,77 +1,138 @@
 import React from 'react'
+import  { useState, useEffect } from "react"
+import './Layout.css'
 
 function Header() {
+  
+
+  const [navbar, setNavbar] = useState()
+  const[navFont,setNavfont]=useState(null)
+  const changeBackground = () => {
+    // console.log(window.scrollTop)
+    if (window.scrollY>=1) {
+      setNavbar("black")
+      setNavfont("white")
+    } else {
+      setNavbar("red");
+      setNavfont("black")
+    }
+  }
+  useEffect(() => {
+    changeBackground()
+    // adding the event when scroll change background
+    window.addEventListener("scroll", changeBackground)
+  })
+
   return (
     <div>
-    <>
-  {/* Hello world */}
-  <nav className="navbar navbar-expand-lg bg-body-tertiary mx-auto">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav ms-auto mx-5">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              Home
+      <>
+        <nav className="navbar navbar-expand-lg d-flex justify-content-center fixed-top" style={{backgroundColor:navbar}}>
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+            
             </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
               aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              Dropdown link
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse  justify-content-center" id="navbarNavDropdown">
+              <ul className="navbar-nav  mx-5" >
+                <li className="nav-item px-lg-3 font-weight-bold">
+                  <a className="nav-link active " aria-current="page" href="#" style={{color:navFont}}>
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item px-lg-3"  px-lg-3>
+                  <a className="nav-link" href="#"style={{color:navFont}}>
+                   Contact
+                  </a>
+                </li>
+                <li className="nav-item px-lg-3"  >
+                  <a className="nav-link" href="#"style={{color:navFont}}>
+                   Blog
+                  </a>
+                </li>
+                <li className="nav-item  px-lg-3">
+                  <a className="nav-link" href="#"style={{color:navFont}}>
+                    About
+                  </a>
+                </li>
+                <li className="nav-item  px-lg-3">
+                  <a className="nav-link" href="#"style={{color:navFont}}>
+                   New Launch 
+                  </a>
+                </li>
+                <li className="nav-item dropdown  px-lg-3">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    style={{color:navFont}}
+                  >
+                   Buy
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#" >
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#" >
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item dropdown  px-lg-3">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    style={{color:navFont}}
+                  >
+                   Rent
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#" >
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#" >
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </>
     </div>
-  </nav>
-</>
-</div>
   )
 }
 
